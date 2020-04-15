b7a39f7144c176eb4207366b0dad794751c20159
cornel <ghita_cornel@yahoo.com>
2020-04-15 23:12:48 +0300

    xxx

--- src/test/java/tests/local/TestSingleNode.java
+++ src/test/java/tests/local/TestSingleNode.java
@@ -1,21 +1,37 @@
 package tests.local;
 
 import local.node.ServerNode;
-import org.apache.ignite.Ignite;
-import org.apache.ignite.IgniteCache;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.client.ClientCache;
+import org.apache.ignite.client.IgniteClient;
+import org.apache.ignite.configuration.ClientConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class TestSingleNode {
 
     private static ServerNode node;
+    private static IgniteClient client;
 
     @BeforeClass
     public static void setUpAll() {
-        IgniteConfiguration configuration = new IgniteConfiguration();
-        configuration.setIgniteInstanceName("1");
-        node = new ServerNode(configuration);
-        node.startNode();
+
+        {
+            IgniteConfiguration configuration = new IgniteConfiguration();
+            configuration.setIgniteInstanceName("1");
+            node = new ServerNode(configuration);
+            node.startNode();
+        }
+
+        {
+            ClientConfiguration clientConfiguration = new ClientConfiguration();
+            clientConfiguration.setAddresses("127.0.0.1");
+            client = Ignition.startClient(clientConfiguration);
+        }
+
     }
 
     @AfterClass
@@ -24,19 +40,21 @@ public class TestSingleNode {
     }
 
     @Test
-    public void testSingleRead() {
+    public void testSingleWriteRead() {
 
-        Ignite ignite = node.getIgnite();
-        IgniteCache<String, Integer> cache = ignite.getOrCreateCache("myCache");
+        ClientCache<Object, Object> cache = client.getOrCreateCache("myCache");
 
-        cache.put("one", 1);
-        cache.put("two", 2);
-        cache.put("three", 3);
+        // write
+        {
+            cache.put("one", 1);
+            cache.put("two", 2);
+            cache.put("three", 3);
+        }
 
-
-        Assert.assertEquals(Integer.valueOf(1), cache.get("one"));
-        Assert.assertEquals(Integer.valueOf(2), cache.get("two"));
-        Assert.assertEquals(Integer.valueOf(3), cache.get("three"));
+        // read and test
+        Assert.assertEquals(1, cache.get("one"));
+        Assert.assertEquals(2, cache.get("two"));
+        Assert.assertEquals(3, cache.get("three"));
 
     }
 
